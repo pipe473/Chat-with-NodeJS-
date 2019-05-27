baa4feb9cfdf59db192770c29199a508ec115725
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-27 17:49:31 +0200

    Agrego los elementos del DOM

--- Public/chat.js
+++ Public/chat.js
@@ -1,1 +1,10 @@
 const socket = io();
+
+//DOM Elements
+
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
