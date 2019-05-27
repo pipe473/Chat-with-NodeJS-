96f7f6977ef25494d4f83cf34e5466c288ecb8ad
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-27 17:14:41 +0200

    conexion del front con el servidor

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const SocketIO = require('socket.io');
-const io = SocketIO(server);
-
-
 //settings
 
 app.set('port', process.env.PORT || 3000);
@@ -16,11 +12,29 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname,'Public')));
 
 
+
 //Start the Server
 
 const server = app.listen(app.get('port'), () => {
 
-console.log('Servidor Activado en Puerto', app.get('port'));
+console.log('Servidor Activado en Puerto', app.get('port'));    
+    
+    });  
+
+
+
+//webSockets
+
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', () => {
+    console.log('New connection');  
 
 
 });
+
+
+
+
+
