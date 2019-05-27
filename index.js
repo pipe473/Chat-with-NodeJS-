3c27cc6f7b69a2bc67f5d760a547172f92a4d157
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-27 16:08:31 +0200

    iniciando servidor

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const app = express();
 //settings
 
 app.set('port', process.env.PORT || 5000);
+
+
+//Start the Server
+
+app.listen(app.get('port'), () => {
+
+console.log('Servidor Activado en Puerto', app.get('port'));
+
+
+}
